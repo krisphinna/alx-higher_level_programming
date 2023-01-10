39a2ff77951e272787434a0db3694652e6463bab
krisphinna <roykriz@yahoo.com>
2023-01-09 18:20:28 -0800

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -12,3 +12,11 @@ def print_matrix_integer(matrix=[[]]):
                     print("{}".format(matrix[r][c]), end=" ")
     else:
         print()
+
+if __name__ == '__main__':
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    print_matrix_integer(matrix)
