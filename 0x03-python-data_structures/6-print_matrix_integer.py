a5f9a68d66b4966ae6242df71f7530119eb16f11
krisphinna <roykriz@yahoo.com>
2023-01-09 18:15:18 -0800

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix[0]) != 0:
+        rows = len(matrix)
+        columns = len(matrix[0])
+        for r in range(rows):
+            for c in range(columns):
+                if c == columns - 1:
+                    print("{}".format(matrix[r][c]))
+                else:
+                    print("{}".format(matrix[r][c]), end=" ")
+    else:
+        print()
