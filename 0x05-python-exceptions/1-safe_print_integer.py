9c13e102611b1e7875b6750e415e1df5dc809f37
krisphinna <roykriz@yahoo.com>
2023-01-24 15:43:19 -0800

    task 1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """
+    prints a list of anything, but only prints the integers
+    Returns the amount of integers printed
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception:
+        return False
