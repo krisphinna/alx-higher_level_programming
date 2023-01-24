5eb38aeb463235293d487f88566d59c116f9b7a5
krisphinna <roykriz@yahoo.com>
2023-01-24 05:43:24 -0800

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
+"""
+Created on Tue Jan 24 14:42:50 2023
+@author: Robinson Montes
+"""
 class Square:
     """Empty class Square that defines a square
 
