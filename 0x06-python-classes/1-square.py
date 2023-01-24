9e66a9370d87ce64e22a35a804d7c8fb2f2eb195
krisphinna <roykriz@yahoo.com>
2023-01-24 06:04:19 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 14:21:54 2020
+@author: Robinson Montes
+"""
+
+class Square:
+    def __init__(self, size):
+        self.__size = size
