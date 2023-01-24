2b65bd2b85ca16f5193080e29159f898183028f4
krisphinna <roykriz@yahoo.com>
2023-01-24 06:20:58 -0800

    task 2

---
+++ 0x06-python-classes/2-sqaure.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 14:21:54 2020
+@author: Robinson Montes
+"""
+
+class Square:
+    """Class Square that has attributes. Instantiation with size
+    Attributes:
+        size: The size of the square
+    """
+    def __init__(self, size=0):
+        """The __init__ method for Square class
+        Args:
+            size: (:obj: 'int', optional): A private instance size
+        Raises:
+            TypeError: Exception if size is not an integer
+            ValueError: Exception if size is less than 0
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
