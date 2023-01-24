a1dad71af97a8cc14daa32ebda8cf8c334e8e1c1
krisphinna <roykriz@yahoo.com>
2023-01-24 06:12:11 -0800

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,5 +6,15 @@ Created on Tue May 19 14:21:54 2020
 """
 
 class Square:
+    """Class Square that has attributes. Instantiation with size
+
+    Attributes:
+        size (int): The size of the square
+    """
     def __init__(self, size):
+        """The __init__ method for Square class
+
+        Args:
+            size: (:obj: 'int'): A private instance size
+        """
         self.__size = size
