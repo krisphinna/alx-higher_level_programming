ddb4de813caa6f1cdfbc4f854354f3d3863b1eef
krisphinna <roykriz@yahoo.com>
2023-01-24 16:12:39 -0800

    task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """
+    divides two integers and prints the result
+    catches divide by zero exception
+    """
+    try:
+        res = a / b
+    except ZeroDivisionError:
+        res = None
+    finally:
+        print("Inside result: {}".format(res))
+    return res
