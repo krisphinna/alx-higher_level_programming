52978123772c5277c1911e1a1983aa56b382aa74
krisphinna <roykriz@yahoo.com>
2023-02-07 16:25:23 -0800

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 07 15:13:37 2023
+@author: Robinson Montes
+"""
+
+
+def lookup(obj):
+    """Returns all objects in an objects dictionary as a list"""
+    return dir(obj)
