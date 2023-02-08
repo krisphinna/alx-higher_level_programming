d2047fce07c3475f6c7ba213994d2c7b7d4f974b
krisphinna <roykriz@yahoo.com>
2023-02-07 17:40:41 -0800

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Web Feb 07 15:13:37 2023
+@author: Robinson Montes
+"""
+
+
+class BaseGeometry():
+    """An empty class"""
+    pass
+
+    def area(self):
+        """Public instance method that calculates the area"""
+        raise Exception("area() is not implemented")
