f35c69fe0d71dea623a40b8e709d496853815d66
krisphinna <roykriz@yahoo.com>
2023-03-15 18:33:12 -0700

    task 20

---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,6 @@
+-- Displays max state temperature
+-- DDL query that displays the max temperature of each state, stored by state
+SELECT state,MAX(value) AS max_temp
+FROM temperatures
+GROUP BY state
+ORDER BY state;
