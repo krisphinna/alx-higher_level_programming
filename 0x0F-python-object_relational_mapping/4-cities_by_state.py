d812336f31c2a6e0a998975170928b92362e0025
krisphinna <roykriz@yahoo.com>
2023-04-18 03:57:38 -0700

    task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Created on Tue April 18 09:05:11 2023
+@author: Kris Adelowo
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 4:
+        print("Usage: {} username password database_name".format(args[0]))
+        exit(1)
+    username = args[1]
+    password = args[2]
+    data = args[3]
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwd=password, db=data, port=3306)
+    cur = db.cursor()
+    num_rows = cur.execute("SELECT cities.id, cities.name, states.name\
+                           FROM cities INNER JOIN states\
+                           ON cities.state_id=states.id\
+                           ORDER BY cities.id;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
