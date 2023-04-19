cbbc1a3add7142c13b7d4a961e7a0c4573e6569d
krisphinna <roykriz@yahoo.com>
2023-04-19 07:02:58 -0700

    task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Created on Wed Apr 19 09:05:11 2023
+@author: Kris Adelowo
+"""
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 5:
+        print("Usage: {} username password database_name".format(args[0]))
+        exit(1)
+    username = args[1]
+    password = args[2]
+    data = args[3]
+    state_name = args[4]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, data))
+    # create custom session object class from database engine
+    Session = sessionmaker(bind=engine)
+    # create instance of new custom session class
+    session = Session()
+    states = session.query(State).filter(State.name == state_name)\
+                    .order_by(State.id)
+    if states is not None and states.count() > 0:
+        for state in states:
+            print('{}'.format(state.id))
+    else:
+        print('Not found')
